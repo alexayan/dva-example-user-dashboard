76f006c98e768b36068c8edf1fe3464d875961a2
alexayan <773655223@qq.com>
2018-04-09 23:16:32 +0800

    适配 dva@2.00, react-router@4.0.0

--- server/createApp.js
+++ server/createApp.js
@@ -1,15 +1,16 @@
-import React from 'react';
 import dva from 'dva';
-import { RouterContext } from 'dva/router';
 
 function createApp(opts) {
   const app = dva(opts);
 
-  app.model(require('../src/models/users'));
+  app.model({ ...require('../src/models/users') });
+  app.model({ ...{
+    namespace: 'app',
+    state: {
 
-  app.router(({ renderProps }) => {
-    return <RouterContext history {...renderProps} />;
-  });
+    },
+  } });
+
   return app;
 }
 
--- src/models/users.js
+++ src/models/users.js
@@ -53,7 +53,7 @@ export default {
   },
   subscriptions: {
     setup({ dispatch, history }) {
-      return history.listen(({ pathname, query }) => {
+      return history.listen(({ pathname, query = {} }) => {
         if (pathname === '/users') {
           dispatch({ type: 'fetch', payload: query });
         }
